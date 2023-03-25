d15caa0c4e9117a47905124098529a8a6d660b86
Xujuan Chen <xchen022@citymail.cuny.edu>
2023-03-24 21:58:07 -0400

    init

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ The following **required** functionality is completed:
 - [ ] **The list displays a list of data fetched using an API call**
 - [ ] **Data uses the useEffect React hook and async/await syntax**
 - [ ] **The app dashboard includes at least three summary statistics about the data such as**
-  - [ ] *insert details*
+- [ ] *insert details*
 - [ ] **A search bar allows the user to search for an item in the fetched data**
 - [ ] **Multiple different filters (2+) allow the user to filter items in the database by specified categories**
 
--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,16 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import CoinInfo from "./Components/coinInfo";
+import SideNav from "./Components/sideNav";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mySecret = import.meta.env.VITE_API_KEY;
+  const url =
+    "https://min-api.cryptocompare.com/data/all/coinlist?&api_key=" + mySecret;
 
+  const [list, setList] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) =>
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setList(json);
+    };
+    fetchAllCoinData().catch(console.error);
+  }, []);
+  console.log(list.Data);
   return (
-    <div className="App">
+    <div className="whole-page">
+      <SideNav />
 
+      <div>
+      <h1>My Crypto List</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      <ul>
+        {searchInput.length > 0
+          ? filteredResults.map((coin) =>
+              list.Data[coin].PlatformType === "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+              ) : null
+            )
+          : list &&
+            Object.entries(list.Data).map(([coin]) =>
+              list.Data[coin].PlatformType === "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+              ) : null
+            )}
+      </ul>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
